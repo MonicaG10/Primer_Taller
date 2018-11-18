3cfb3ab73f60600bc9c82cab6e164b49c06fc4f1
Edison Schneider Hincapie Ramirez <schi1989@gmail.com>
2018-11-17 21:48:55 -0500

    uber

--- Uber2/Script 1 al 7.sql
+++ Uber2/Script 1 al 7.sql
@@ -1,4 +1,4 @@
-/*1.Crear una vista llamada â€œMEDIOS_PAGO_CLIENTESâ€ que contenga las siguientes columnas:
+/*1.Crear una vista llamada â€œMEDIOS_PAGO_CLIENTESâ€? que contenga las siguientes columnas:
 CLIENTE_ID, NOMBRE_CLIENTE (Si tiene el nombre y el apellido separados en columnas, deberÃ¡n
 estar unidas en una sola), MEDIO_PAGO_ID, TIPO (TDC, Android, Paypal, Efectivo),
 DETALLES_MEDIO_PAGO, EMPRESARIAL (FALSO o VERDADERO), NOMBRE_EMPRESA (Si la
@@ -20,7 +20,7 @@ INNER JOIN METODOS_PAGOS B ON A.ID=B.cliente_id ;
 
 
 /*2. Cree una vista que permita listar los viajes de cada cliente ordenados cronolÃ³gicamente. El nombre
-de la vista serÃ¡ â€œVIAJES_CLIENTESâ€, los campos que tendrÃ¡ son: FECHA_VIAJE,
+de la vista serÃ¡ â€œVIAJES_CLIENTESâ€?, los campos que tendrÃ¡ son: FECHA_VIAJE,
 NOMBRE_CONDUCTOR, PLACA_VEHICULO, NOMBRE_CLIENTE, VALOR_TOTAL,
 TARIFA_DINAMICA (FALSO O VERDADERO), TIPO_SERVICIO (UberX o UberBlack),
 CIUDAD_VIAJE. (0.25)*/
@@ -44,4 +44,55 @@ INNER JOIN DETALLE_FACTURAS G ON E.ID=G.FACTURA_ID
 INNER JOIN CIUDADES H ON A.CIUDAD_ID=H.ID
 ORDER BY FECHA_INICIO_RECORRIDO
 ;
+/*3 Cree y evidencie el plan de ejecución de la vista VIAJES_CLIENTES. Cree al menos un índice donde                 
+mejore el rendimiento del query y muestre el nuevo plan de ejecución. ?(0.25) 
+*/
+EXPLAIN PLAN SET STATEMENT_ID = 'EXPLAIN_PLAN_VIAJES_CLIENTES' FOR
+SELECT * FROM VIAJES_CLIENTES;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY); 
+
+
+/*5 Crear una función llamada VALOR_DISTANCIA que reciba la distancia en kilómetros y el nombre de              
+la ciudad donde se hizo el servicio. Con esta información deberá buscar el valor por cada kilómetro                 
+dependiendo de la ciudad donde esté ubicado el viaje. Deberá retornar el resultado de multiplicar la               
+distancia recorrida y el valor de cada kilómetro dependiendo de la ciudad. Si la distancia es menor a 0                   
+kilómetros o la ciudad no es válida deberá levantar una excepción propia. Ejemplo: Viaje_ID: 342 que                
+fue hecho en Medellín y la distancia fue 20.68km. En este caso deberá retornar 20.68 * 764.525994 =15810.3976. ?(0.25) */
+
+CREATE OR REPLACE FUNCTION VALOR_DISTANCIA (DIS_KILOMETRO NUMBER, CIUDAD VARCHAR) 
+RETURN NUMBER
+IS
+VALOR_KILOMETRO NUMBER;
+BEGIN
+IF DIS_KILOMETRO > 0 THEN
+SELECT TARIFA_KILOMETRO INTO VALOR_KILOMETRO FROM CIUDADES WHERE DESCRIPCION = CIUDAD;
+RETURN (DIS_KILOMETRO * VALOR_KILOMETRO) ;
+ELSE 
+DBMS_OUTPUT.PUT_LINE('Distancia no valida');
+END IF;
+END;
+--Ejecutamos la consulta funcion
+SELECT VALOR_DISTANCIA(10,'Bogota') AS Valor FROM DUAL;
+
+/*6Crear una función llamada VALOR_TIEMPO que reciba la cantidad de minutos del servicio y el               
+nombre de la ciudad donde se hizo el servicio. Con esta información deberá buscar el valor por cada
+minuto dependiendo de la ciudad donde esté ubicado el viaje. Deberá retornar el resultado de               
+multiplicar la distancia recorrida y el valor de cada minuto dependiendo de la ciudad. Si la cantidad de                  
+minutos es menor a 0 o la ciudad no es válida deberá levantar una excepción propia. Ejemplo:                 
+Viaje_ID: 342 que fue hecho en Medellín y el tiempo fue 28 minutos. En este caso deberá retornar 28* 178.571429 = 5000.00001 ?(0.25) */
+
+CREATE OR REPLACE FUNCTION VALOR_TIEMPO (CANT_MINUTOS NUMBER, CIUDAD VARCHAR) 
+RETURN NUMBER
+IS
+VALOR_MINUTO NUMBER;
+BEGIN
+IF CANT_MINUTOS > 0  THEN
+SELECT TARIFA_MINUTO INTO VALOR_MINUTO FROM CIUDADES WHERE DESCRIPCION = CIUDAD;
+RETURN(CANT_MINUTOS * VALOR_MINUTO);
+ELSE
+DBMS_OUTPUT.PUT_LINE('Distancia no valida');
+END IF;
+END;
+--Ejecutamos la consulta funcion
+SELECT VALOR_TIEMPO(60,'Medellin') AS Valor FROM DUAL;
 
